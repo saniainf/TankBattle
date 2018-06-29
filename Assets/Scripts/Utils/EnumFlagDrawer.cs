4d5dfb566009383842f711e03b775ef1845b6e99
sania <saniainf@gmail.com>
2018-06-29 15:38:41 +0700

    1231414

--- Assets/Scripts/Utils/EnumFlagDrawer.cs
+++ Assets/Scripts/Utils/EnumFlagDrawer.cs
@@ -1,13 +1,13 @@
-using System;
-using System.Reflection;
-using UnityEditor;
-using UnityEngine;
+//using System;
+//using System.Reflection;
+//using UnityEditor;
+//using UnityEngine;
 
-[CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
-public class EnumFlagsAttributeDrawer : PropertyDrawer
-{
-    public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label)
-    {
-        _property.intValue = EditorGUI.MaskField(_position, _label, _property.intValue, _property.enumNames);
-    }
-}
+//[CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
+//public class EnumFlagsAttributeDrawer : PropertyDrawer
+//{
+//    public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label)
+//    {
+//        _property.intValue = EditorGUI.MaskField(_position, _label, _property.intValue, _property.enumNames);
+//    }
+//}
